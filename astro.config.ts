b12259f0e90fca42a3e6bcc91d2a0b5705ba19c6
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-08-28 16:51:06 -0300

    fix: 🐛 removes useless redirect

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://ecovidapangeia.com/",
+  site: "https://ecovidapangeia.com",
   trailingSlash: "never",
-  redirects: {
-    "/": "",
-  },
 });
